35537abee44acdea85aae51ed4d763577b142334
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2023-11-08 15:28:28 +0100

    add: snack1

---
+++ snack1/js/script.js
@@ -0,0 +1,45 @@
+//array di oggetti contenente delle bici
+const bikes = [
+    {
+        nome:"Scott",
+        peso: 11.28 ,
+    },
+    {
+        nome:"Cannondale",
+        peso: 11.1,
+    },
+    {
+        nome:"Genesis",
+        peso: 11.3,
+    },
+    {
+        nome:"MTB Focus",
+        peso: 5.9,
+    }
+]
+//debug
+console.log(bikes);
+//variabile con all'interno un array che ha l'oggeto della bici piú leggera
+const biciLeggera = confrontoPeso(bikes);
+console.log(biciLeggera,"bici piú leggera");
+
+//inserisco il testo nel dom con scritta la bici piú leggera
+const text = document.getElementById("message");
+text.innerHTML = `
+    <h1>Ecco la bici da corsa piú leggera</h1>
+    <h2> Nome: ${biciLeggera[0].nome}</h2>
+    <h2> Peso: ${biciLeggera[0].peso}</h2>
+    `
+
+
+
+/**
+ * funzione per trovare la bici piú leggera dell'array
+ * @param {Array} bikes array di object contenente le bici da corsa
+ * @returns {Array} array contentente l'object della bici da corsa piú leggera ( < 10)
+ */
+function confrontoPeso(bikes) {
+    //variabile per il return, che avrá il valore della bici piú leggera
+    const result = bikes.filter((curBike) => curBike.peso < 10 )
+    return result
+}
